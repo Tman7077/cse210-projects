4015423692cdb69661d833e5a3b45c8a6fea077b
Tyler Bartle <tman.bartleboy1@gmail.com>
2024-01-09 11:15:18 -0700

    Added Prep1 James Bond-ifying function

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,6 +8,6 @@ class Program
         string fname = Console.ReadLine();
         Console.WriteLine("What is your last name?");
         string lname = Console.ReadLine();
-        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}");
     }
 }
