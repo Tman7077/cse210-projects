812b4d4148e4003e1d6d257dfe1343dbb0a75ffe
Tyler Bartle <tman.bartleboy1@gmail.com>
2024-02-05 19:31:41 -0700

    Revert "Moved the date from the journal class to the entry class, and added a comment at the top of Program.cs to locate the extra function I added."
    This reverts commit c9643a37039891005093a5da744b559adab6d7d8.

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -3,7 +3,6 @@ public class Entry
 {
     public string _prompt = ""; // the user's prompt
     public string _userEntry = ""; // the user's answer
-    public string _date = DateTime.Now.ToString("dddd, MMMM dd, yyyy"); // current date in readable format
     public void PromptUser(Prompt prompt)
     {
         // generate a prompt and ask the user's input
--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -3,7 +3,8 @@ using System.Diagnostics.Tracing;
 using System.IO;
 public class Journal
 {
-    // filename based on date, and a list of entries
+    // current date in readable format, filename based on date, and a list of entries
+    public string _date = DateTime.Now.ToString("dddd, MMMM dd, yyyy");
     public string _filename = $"journal_{DateTime.Now.ToString("MM-dd-yyyy")}.txt";
     public List<Entry> _entries = new List<Entry>();
 
@@ -20,13 +21,14 @@ public class Journal
         // if there is at least one entry in the current journal
         if (journal._entries is not [])
         {
+            Console.WriteLine($"\n{_date}");
             int i = 1; // to number the entries if numbering is displayed
             if (displayNumbers)
             {
                 // display numbers in front of entries (for entry editing purposes)
                 foreach (Entry entry in _entries)
                 {
-                    Console.WriteLine($"{i}. {entry._date}\n{entry._prompt}\n{entry._userEntry}\n");
+                    Console.WriteLine($"{i}. {entry._prompt}\n{entry._userEntry}\n");
                     i++;
                 }
             }
@@ -35,7 +37,7 @@ public class Journal
                 // display entries normally
                 foreach (Entry entry in _entries)
                 {
-                    Console.WriteLine($"{entry._date}\n{entry._prompt}\n{entry._userEntry}\n");
+                    Console.WriteLine($"{entry._prompt}\n{entry._userEntry}\n");
                 }
             }
         }
@@ -73,11 +75,10 @@ public class Journal
     // save currently loaded entries to a file
     public void Save()
     {
-        List<string> lines = new List<string>(); // list of lines to write, starting with the readable date
+        List<string> lines = new List<string> {_date}; // list of lines to write, starting with the readable date
         // add a prompt line followed by an entry line for each entry in the list
         foreach (Entry entry in _entries)
         {
-            lines.Add(entry._date);
             lines.Add(entry._prompt);
             lines.Add(entry._userEntry);
         }
@@ -98,16 +99,17 @@ public class Journal
         Journal journal = new Journal();
         string[] lines = System.IO.File.ReadAllLines(filename);
         // set the first line to journal's date, and the journal's filename to the file's name
+        string date = lines[0];
+        journal._date = date;
         journal._filename = filename;
         // iterate through lines to load each prompt and entry into their respective Entry objects
-        for (int i = 0; i < lines.Count(); i++)
+        for (int i = 1; i < lines.Count(); i++)
         {
             Entry entry = new Entry();
-            if (i % 3 == 0)
+            if (i % 2 == 1)
             {
-                entry._date = lines[i];
-                entry._prompt = lines[i+1];
-                entry._userEntry = lines[i+2];
+                entry._prompt = lines[i];
+                entry._userEntry = lines[i+1];
                 journal._entries.Add(entry);
             }
         }
