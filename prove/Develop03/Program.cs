b7be24d7823d009486ec1379ab4d7c44ee485762
Tyler Bartle <tman.bartleboy1@gmail.com>
2024-02-08 10:32:11 -0700

    Changed and added a few comments, including the plantuml diagram from the team meeting (which doesn't perfectly match the program, but the concepts are the same)

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,11 +2,14 @@ using System;
 
 class Program
 {
-    // exceeding requirements: in Scripture.cs line 6, there is a dictionary of all Book of Mormon doctrinal mastery
-    // scriptures, from which the program randomly selects for the user to memorize
+    //www.plantuml.com/plantuml/png/RP71IWCn48RlynHxp4AVe8WKB6WFeYZeGMH9ar63sMJ9p5ONwRkRR3Tf3ZqbcVppp_yn6s8PUEmjA2k9cYVym81E8Vn1j_V-UzqG1-C-eLDVSc0CwyOuXkw0WP3cnzixGEaJRNMl3LPDRR1gvi4loMKfeDFdPmjmYrovbXoFCcf4o3-v7WL53Jkw7osRmUBiIAazPh79CrTozED2_go26NWCCpdbHoJS6iN6EnccgqnoEg-XAp7gwHtZBvUyK8n5MzqlTRPHZ1OLYpo_mgi2RWqDLaw9uU2DXeSzgUJ9fbzMadbtTEFxmIAZdUwCrkX4_ihShpxeXFmJp_Eo3PtaYnYIIjII5Y0gwiKSOODEZxtz1m00
+
+    // exceeding requirements: in Scripture.cs line 6, there is a dictionary of all Book of Mormon doctrinal mastery scriptures,
+    // from which the program randomly selects for the user to memorize
     static void Main(string[] args)
     {
         // select a random scripture from the dictionary of scriptures and instantiate it as a Scripture object
+        // Select() could also take an input of a scripture reference to select that scripture specifically
         Scripture scripture = new Scripture().Select();
 
         string userInput; // nothing if the user wants to continue, something (quit, usually), if they want to exit
