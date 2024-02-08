11cff98bf6f2c3eba1d79e20325df27351732917
Tyler Bartle <tman.bartleboy1@gmail.com>
2024-02-08 10:30:04 -0700

    Added an IsHidden method

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -19,4 +19,20 @@ public class Word
         // the regex preserves (verse) numbers and punctuation
         return letters.Replace(_word,"_");
     }
+
+    public bool IsHidden()
+    {
+        Regex numbersOrUnderscores = new Regex("[0-9+|_+]"); // regular expression to match any set of numbers or underscores ("1", "12", "_", and "__" would each match once)
+
+        // if _word has either numbers or underscores in it
+        if (numbersOrUnderscores.IsMatch(_word))
+        {
+            return true;
+        }
+        // if _word has neither numbers nor underscores in it
+        else
+        {
+            return false;
+        }
+    }
 }
